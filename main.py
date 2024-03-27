50e4089ce9d6e81b251ddba0acae1a3d0df0ee27
ImAgiD <daurbekoaajgerim@gmail.com>
2024-03-27 15:11:39 +0600

    fastapi 2

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 "26.03.24==================Fastapi=================="
 from fastapi import FastAPI
 
+from app.views import router
+
 app = FastAPI()
-
-@app.post('/create')
-def create_product():
-    return {"message":"Продукт успешно создался"}
-
-@app.get('/')
-def get_products():
-    return {"message":"Вот ваши продукты"}
-
-@app.delete("/delete")
-def delete_product():
-    return {"message":"Продукт удален"}
-
+app.include_router(router)
--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 from decouple import config
 
 #db_url = "driver://db_user:password@db_host/db_name" создаем 
-db_url = f'postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}@{config('DB_HOST')}/{config('DB_NAME')}'
+db_url = f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}@{config('DB_HOST')}/{config('DB_NAME')}"
 
 engine = create_engine(db_url)
 
